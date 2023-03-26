952c8705c3721c4680fda5c792a6402989501ec0
washington.anjos <washington.anjos_ntconsult@prestador.globo>
2023-03-26 18:48:17 -0300

    Configuração do projeto

--- src/main/java/br/com/vrbeneficios/autorizador/AutorizadorApplication.java
+++ src/main/java/br/com/vrbeneficios/autorizador/AutorizadorApplication.java
@@ -2,8 +2,10 @@ package br.com.vrbeneficios.autorizador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "br/com/vrbeneficios/autorizador/model")
 public class AutorizadorApplication {
 
 	public static void main(String[] args) {
